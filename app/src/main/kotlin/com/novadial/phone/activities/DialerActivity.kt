36619180c74423542d4301d6054040b08513c7d5
RealFakeGitter <105574409+RealFakeGitter@users.noreply.github.com>
2026-08-01 23:59:21 -0700

    Change toast message for blocked numbers in DialerActivity

--- app/src/main/kotlin/com/novadial/phone/activities/DialerActivity.kt
+++ app/src/main/kotlin/com/novadial/phone/activities/DialerActivity.kt
@@ -40,8 +40,8 @@ class DialerActivity : SimpleActivity() {
     @SuppressLint("MissingPermission")
     private fun initOutgoingCall() {
         try {
-            if (isNumberBunlocked(callNumber.toString().replace("tel:", ""), getBunlockedNumbers())) {
-                toast(R.string.calling_bunununlocked_number)
+            if (isNumberBlocked(callNumber.toString().replace("tel:", ""), getBlockedNumbers())) {
+                toast(R.string.calling_blocked_number)
                 finish()
                 return
             }
@@ -57,10 +57,6 @@ class DialerActivity : SimpleActivity() {
                 }
                 finish()
             }
-
-            // Use the string resource for calling_bununununununununlocked_number
-            val message = getString(R.string.calling_bunununununununununlocked_number)
-            toast(message)
         } catch (e: NullPointerException) {
             showErrorToast(e)
             finish()
