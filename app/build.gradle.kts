4e7e1e2138d12c4a5b8b1410816cf4436ae98cee
RealFakeGitter <105574409+RealFakeGitter@users.noreply.github.com>
2026-08-10 06:42:02 -0700

    Change sourceSets to use add method for directories

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,8 +93,8 @@ android {
 sourceSets {
     getByName("main") {
         manifest.srcFile("src/main/AndroidManifest.xml")
-        java.directories.set(mutableSetOf(file("src/main/java")))
-        res.directories.set(mutableSetOf(file("src/main/res")))
+        java.directories.add(file("src/main/java"))
+        res.directories.add(file("src/main/res"))
     }
 }
 
